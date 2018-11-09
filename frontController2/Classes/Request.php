89e3d41d6f06a4708934a1c0263dfc71c9d913e6
Olivier Wenzek <olivier.wenzek@online-verlag-freiburg.de>
2018-11-09 10:50:56 +0100

    first class Request, json + gitignore files

---
+++ frontController2/Classes/Request.php
@@ -0,0 +1,44 @@
+<?php
+	/**
+	 * Created by PhpStorm.
+	 * User: wenzek
+	 * Date: 09.11.18
+	 * Time: 10:43
+	 */
+
+	namespace Classes\Request;
+
+
+	class Request
+	{
+		public function __construct( $pUri, $pParams)
+		{
+			$this->mUri = $pUri;
+			$this->mParams = $pParams;
+		}
+
+		public function getUri(  ) : string
+		{
+			return $this->mUri;
+		}
+
+		public function setParam( $pKey, $pValue ) : Request
+		{
+			$this->mParams[$pKey] = $pValue;
+			return $this;
+		}
+
+		public function getParam( $pKey )
+		{
+			if( ! isset( $this->mParams[$pKey])) {
+				throw new \InvalidArgumentException( 'The request parameter with the key ' . $pKey . ' is invalid');
+			}
+
+			return $this->mParams[$pKey];
+		}
+
+		public function getParams() : array
+		{
+			return $this->mParams;
+		}
+	}
